a0e32e55e3250e04ad238a9f418699590a7fbe38
Inbal Ilsar <inbal16@meet.mit.edu>
2015-02-26 20:00:14 +0200

    GOAT

---
+++ Tiger.py
@@ -0,0 +1,18 @@
+class PieceWhite(Turtle)
+	def __init__(self,cell,color):
+	RawTurtle.__init__(self,cell)
+	self.color = color
+	self.cell = cellnum
+
+
+
+
+class PieceBlack(Turtle)
+	def __init__(self,cell,color):
+	RawTurtle.__init__(self,cell)
+	self.color = color
+	self.cell = cellnum
+
+	def checkRight(self)
+		#check the right cell.
+		if cell==PieceWhite
