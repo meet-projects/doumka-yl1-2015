26ca97cc97b5de1c9be6601f01ffa3b90420aa03
Musa Abu Al-Hawa <musa16@meet.mit.edu>
2015-03-05 19:42:25 +0200

    Class_object_cells

--- class_cell.py
+++ class_cell.py
@@ -1,9 +1,22 @@
 class Cell:
-	def __init__(self,col,row,color):
+	def __init__(self,col,row,color,piece):
 		self.col = col
 		self.row = row
 		self.piece = None
 		self.color = color
 		
+	def getCol(self):
+		return self.col
+
+	def getRow(self): 
+		return self.raw
+
 	def getColor(self):
-		return
+		return self.color
+		
+	def getPiece(self) :
+		return self.piece
+	
+	def setPiece(self)
+		self.piece = piece
+	
